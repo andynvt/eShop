72e8755d8608d9c5e424bfb82547f58fed9f4ab0
andynvt <taivnguyen@lecle.co.kr>
2022-08-16 12:00:27 +0700

    init

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,23 @@
+const String appName = 'Voux Originals';
+
+const String packageName = 'com.vouxoriginals.user';
+const String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+const String iosPackage = 'com.vouxoriginals.user';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://vouxoriginals.page.link';
+const String deepLinkName = 'Voux Originals';
+
+const int timeOut = 50;
+const int perPage = 50;
+
+
+const String baseUrl = 'https://vouxoriginals.in/app/v1/api/';
+
+
+const String jwtKey = "65c9cd19cd138f19ddf2f6320c7a802ee936c548";
+
+
+
